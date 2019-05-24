a5b6262cadee8d61d35fd25bc3dd16dc8dc7cfd0
Renan Andrades <renanandrades@hotmail.com>
2019-05-24 17:37:38 -0300

    semifinal1

--- Untitled.py
+++ Untitled.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[48]:
+# In[22]:
 
 
 from __future__ import print_function
 
+from joblib import dump
 import pandas as pd 
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pickle
 from matplotlib.mlab import griddata
 
 import sys
@@ -45,7 +47,8 @@ def plotCorrWithClass(ini, end, class_name):
 	plt.tight_layout()
 	plt.show()
 
-data = pd.read_csv('data_arrhythmia.csv', sep = ';', na_values='?')
+data1 = pd.read_csv('data_arrhythmia.csv', sep = ';', na_values='?')
+data = data1.drop(data1[(data1['height'] > 250)].index) #retirei os valores invalidos de altura (700m)
 
 for col in data.columns:
 	num_null = data[col].isnull().sum()
@@ -64,6 +67,12 @@ data.fillna(data.mean(), inplace = True)
 data.loc[data.diagnosis == 1, 'diagnosis'] = 0
 data.loc[data.diagnosis  > 1, 'diagnosis'] = 1
 
+print ('\nBanco com %d amostras e %d colunas\n' % data.shape)
+#data.target.shape
+
+print ("Tabela sumarizando as colunas do banco\n")
+print (data.describe())
+
 X = data.loc[:, data.columns != 'diagnosis']
 y = data['diagnosis']
 
@@ -71,47 +80,58 @@ from sklearn.ensemble import RandomForestClassifier as RF
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.base import clone
+from sklearn.model_selection import cross_val_score
+#from joblib import dump, load    (No module named joblib)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
 
 best_score = float('-inf')
 
-plot_3d1 = []
+"""
+file_rf = open("Tuning_rf_acc.csv","w")
+
+plot_3d = []
 print("Testing Random Forests")
 for max_depth in (1, 2, 3, 4, 5): 
 	for n_estimators in [10, 25, 50, 100, 200]:
-		for min_samples_split in [2, 3, 4, 5]:
-			for min_samples_leaf in [0.5, 1, 2, 4, 8]:
-				print(max_depth, n_estimators, min_samples_split, min_samples_leaf, end= ' ')
-				clf = RF(n_estimators = n_estimators, max_depth = max_depth, min_samples_split = min_samples_split, min_samples_leaf = min_samples_leaf).fit(X_train, y_train)
+		for max_features in [2, 4, 6, 8, 10, 15, 25, 50, "auto"]:
+			for min_samples_split in [2, 4, 8, 16, 32]:
+				print(max_depth, n_estimators, max_features, min_samples_split, end= ' ')
+				clf = RF(max_depth = max_depth, n_estimators = n_estimators, max_features = max_features, min_samples_split = min_samples_split).fit(X_train, y_train)
 				score_tr, score_te = printAccuracy(clf, X_train, X_test)
 				if score_te > best_score:
 					best_score = score_te
 					best_rf = clone(clf)
                     
 				#acc = printAccuracy(clf, X_train, X_test)
-				plot_3d1.append([n_estimators, max_depth, best_rf])                    
-                    
-plot_3d1 = np.array(plot_3d1)
+				plot_3d.append([n_estimators, max_depth, score_te])                    
+				rf_cv = cross_val_score(clf, X_train, y_train, cv=5)
+				print (max_depth, n_estimators, max_features, min_samples_split, rf_cv.mean(), score_te, file=file_rf)
+                
+dump( best_rf, 'best_rf.pkl')     
+
+                
+plot_3d = np.array(plot_3d)
 from mpl_toolkits.mplot3d import Axes3D 
 
-fig1 = plt.figure()
-ax1 = fig1.gca(projection='3d')
+fig = plt.figure()
+ax = fig.gca(projection='3d')
 
-pltX1, pltY1 = plot_3d1[:,0], plot_3d1[:,1]
-pltZ1 = plot_3d1[:,-1]
+pltX, pltY = plot_3d[:,0], plot_3d[:,1]
+pltZ = plot_3d[:,-1]
 
-#df = pd.DataFrame({'x': pltX, 'y': pltY, 'z': pltZ}, index=range(len(pltX)))
+df = pd.DataFrame({'x': pltX, 'y': pltY, 'z': pltZ}, index=range(len(pltX)))
 
-surf1 = ax1.plot_trisurf(df.x, df.y, df.z, cmap='jet', linewidth=0.1, edgecolor='k')
-ax1.set_xlabel('Max_depth')
-ax1.set_ylabel('N estimators')
-ax1.set_zlabel('Test accuracy')
-fig1.colorbar(surf1, shrink=0.5, aspect=5)
+surf = ax.plot_trisurf(df.x, df.y, df.z, cmap='jet', linewidth=0.1, edgecolor='k')
+ax.set_xlabel('N estimators')
+ax.set_ylabel('max_depth')
+ax.set_zlabel('Test accuracy')
+fig.colorbar(surf, shrink=0.5, aspect=5)
 
 plt.show()
-                    
-                    
+file_rf.close()
+"""    
+    
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
 
@@ -126,76 +146,95 @@ from sklearn.svm import SVC
 
 best_score = float('-inf')
 
-plot_3d2 = []
+file_svm = open("Tuning_svm_acc.csv","w")
+
+#plot_3d = []
 print("\nTesting SVM")
 for c in [0.125,0.25, 0.5, 1, 2, 4, 8, 16]:
-	for g in [1, 2, 3, 4, 5, 'auto']:
+	for g in [0.0625, 0.125, 0.25, 0.5, 2, 4, 8, 16, 32, 'auto']:
 		for tol in [0.001, 0.006, 0.011, 0.016]:
-			for coef0 in [0, 0.01, 0.02, 0.03]:
-				clf = SVC(C=c, tol = tol, coef0 = coef0, kernel = 'rbf', gamma = g).fit(X_train_scaled, y_train)
-				print(c, g, tol, coef0, end= ' ')
-				score_tr, score_te = printAccuracy(clf, X_train_scaled, X_test_scaled)
-				if score_te > best_score:
-					best_score = score_te
-					best_svm = clone(clf)
-
-					plot_3d2.append([c, tol, best_svm])  
+			clf = SVC(C=c, gamma = g, tol = tol, kernel = 'rbf').fit(X_train_scaled, y_train)
+			print(c, g, tol, end= ' ')
+			score_tr, score_te = printAccuracy(clf, X_train_scaled, X_test_scaled)
+			if score_te > best_score:
+				best_score = score_te
+				best_svm = clone(clf)
                     
-plot_3d2 = np.array(plot_3d2)
-from mpl_toolkits.mplot3d import Axes3D 
+			#acc = printAccuracy(clf, X_train, X_test)
+			#plot_3d.append([c, g, score_te])
+			svm_cv = cross_val_score(clf, X_train_scaled, y_train, cv=5)                
+			print (c, g, tol, svm_cv.mean(), score_te, file=file_svm)
 
-fig2 = plt.figure()
-ax2 = fig2.gca(projection='3d')
+dump(best_svm, 'best_svm.pkl')            
 
-pltX2, pltY2 = plot_3d2[:,0], plot_3d2[:,1]
-pltZ2 = plot_3d2[:,-1]
-
-surf2 = ax2.plot_trisurf(df.x, df.y, df.z, cmap='jet', linewidth=0.1, edgecolor='k')
-ax2.set_xlabel('C')
-ax2.set_ylabel('Tol')
-ax2.set_zlabel('Test Accuracy')
-fig2.colorbar(surf2, shrink=0.5, aspect=5)
-
-plt.show(2)
-                                        
-
-    
-from sklearn.ensemble import GradientBoostingClassifier as GBC
-
-best_score = float('-inf')
-
-plot_3d3 = []
-print("\nTesting GBC")
-for learning_rate in [0.0125, 0.025, 0.05, 0.1, 0.5, 1 ]:
-	for n_estimators in [10, 25, 50, 100, 200]:
-		for max_depth in [1, 2, 3, 4, 5]:
-			for max_leaf_nodes in [2, 5, 10, 20, 25]:
-				clf = GBC(learning_rate = learning_rate, n_estimators = n_estimators, max_depth = max_depth, max_leaf_nodes = max_leaf_nodes ).fit(X_train_scaled, y_train)
-				print(learning_rate, n_estimators, max_depth, max_leaf_nodes, end= ' ')
-				score_tr, score_te = printAccuracy(clf, X_train_scaled, X_test_scaled)
-				if score_te > best_score:
-					best_score = score_te
-					best_gbc = clone(clf)
-
-					plot_3d3.append([learning_rate, n_estimators, best_gbc]) 
-                    
-plot_3d3 = np.array(plot_3d3)
+"""
+plot_3d = np.array(plot_3d)
 from mpl_toolkits.mplot3d import Axes3D 
 
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 
-pltX, pltY = plot_3d3[:,0], plot_3d3[:,1]
-pltZ = plot_3d3[:,-1]
+pltX, pltY = plot_3d[:,0], plot_3d[:,1]
+pltZ = plot_3d[:,-1]
 
-surf3 = ax.plot_trisurf(df.x, df.y, df.z, cmap='jet', linewidth=0.1, edgecolor='k')
-ax.set_xlabel('Learning Rate')
-ax.set_ylabel('N estimators')
-ax.set_zlabel('Test Accuracy')
-fig.colorbar(surf3, shrink=0.5, aspect=5)
+df = pd.DataFrame({'x': pltX, 'y': pltY, 'z': pltZ}, index=range(len(pltX)))
 
-plt.show()                    
+surf = ax.plot_trisurf(df.x, df.y, df.z, cmap='jet', linewidth=0.1, edgecolor='k')
+ax.set_xlabel('C')
+ax.set_ylabel('Tol')
+ax.set_zlabel('Test accuracy')
+fig.colorbar(surf, shrink=0.5, aspect=5)
+
+plt.show()
+"""
+file_svm.close()
+
+
+from sklearn.ensemble import GradientBoostingClassifier as GBC
+
+best_score = float('-inf')
+
+file_gbc = open("Tuning_gbc_acc.csv","w")
+
+plot_3d = []
+print("\nTesting GBC")
+for learning_rate in [0.0125, 0.025, 0.05, 0.1, 0.5, 1 ]:
+	for n_estimators in [10, 25, 50, 100, 200]:
+		for max_depth in [1, 2, 3, 4, 5]:
+			clf = GBC(learning_rate = learning_rate, n_estimators = n_estimators, max_depth = max_depth).fit(X_train_scaled, y_train)
+			print(learning_rate, n_estimators, max_depth, end= ' ')
+			score_tr, score_te = printAccuracy(clf, X_train_scaled, X_test_scaled)
+			if score_te > best_score:
+				best_score = score_te
+				best_gbc = clone(clf)
+
+			#acc = printAccuracy(clf, X_train, X_test)
+			plot_3d.append([learning_rate, n_estimators, score_te])
+			gbc_cv = cross_val_score(clf, X_train_scaled, y_train, cv=5)
+			print (learning_rate, n_estimators, max_depth, gbc_cv.mean(), score_te, file=file_gbc)
+
+dump(best_gbc, 'best_gbc.pkl')                
                     
+plot_3d = np.array(plot_3d)
+from mpl_toolkits.mplot3d import Axes3D 
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+pltX, pltY = plot_3d[:,0], plot_3d[:,1]
+pltZ = plot_3d[:,-1]
+
+df = pd.DataFrame({'x': pltX, 'y': pltY, 'z': pltZ}, index=range(len(pltX)))
+
+surf = ax.plot_trisurf(df.x, df.y, df.z, cmap='jet', linewidth=0.1, edgecolor='k')
+ax.set_xlabel('Learning rate')
+ax.set_ylabel('N estimators')
+ax.set_zlabel('Test accuracy')
+fig.colorbar(surf, shrink=0.5, aspect=5)
+
+plt.show()
+file_gbc.close()                    
+
 for col in data.columns[:14]:
 
 	unique = np.unique(data[col])
@@ -216,6 +255,18 @@ plotCorrWithClass(0,14, 'diagnosis')
 
 plotCorrMap(15,26)
 plotCorrWithClass(15,26, 'diagnosis')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
